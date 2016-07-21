a6990d9b73d1325c83870294532ae324a038079a
vthatiko <vthatiko@VTHATIKO.ap.qualcomm.com>
2016-07-21 14:11:53 +0530

    New Class

---
+++ Model/src/com/sample/app/model/Test.java
@@ -0,0 +1,11 @@
+package com.sample.app.model;
+
+public class Test {
+    public Test() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+    }
+}
