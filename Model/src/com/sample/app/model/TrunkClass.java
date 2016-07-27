18e3ee6d113bbdc6c394b138dd4740608be0c8db
sudheer449 <sudheer.thatikonda@outlook.com>
2016-07-27 10:22:57 +0530

    Added TrunkClass and Trunk Two Classes

---
+++ Model/src/com/sample/app/model/TrunkClass.java
@@ -0,0 +1,11 @@
+package com.sample.app.model;
+
+public class TrunkClass {
+    public TrunkClass() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        TrunkClass trunkClass = new TrunkClass();
+    }
+}
