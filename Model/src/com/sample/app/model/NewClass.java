a0b885c4d39704f6a1c921c8d5531960b4f284cf
sudheer449 <sudheer.thatikonda@outlook.com>
2016-07-27 10:50:22 +0530

    New Class  Added

---
+++ Model/src/com/sample/app/model/NewClass.java
@@ -0,0 +1,11 @@
+package com.sample.app.model;
+
+public class NewClass {
+    public NewClass() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        NewClass newClass = new NewClass();
+    }
+}
